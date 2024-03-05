d66777ffa77838c287c4b19eebd5b4abadaa72db
Darren Rose <drose@codehole.xyz>
2024-03-05 15:34:37 -0500

    Fixed issue with dual face cards

--- modules.py
+++ modules.py
@@ -1,20 +1,50 @@
-from json import loads
 from requests import get
-# functions to be used by the routes
+from json import loads
 
-# returns a list of results
-def search_api(source):
+""" def search_api(source):
     api_prefix = "https://api.scryfall.com/cards/search?unique=prints&q="
     
-    try:
-        api_url = api_prefix + f'{source}'
+    api_url = api_prefix + f'{source}'
 
-        card_list = []
+    card_list = []
+    paging_list = []
+    
+    while True:
+        paging_list = loads(get(api_url).text)
 
-        while True:
-            paging_list = loads(get(api_url).text)
+        for card in paging_list['data']:
+            card_data = {
+                'name': card['name'],
+                'set': card['set_name'],
+                'num': card['collector_number'],
+                'price': card['prices']['usd'],
+                'image': card['image_uris']['normal']
+            }
 
-            for card in paging_list['data']:
+            card_list.append(card_data)
+
+        if not paging_list['has_more']:
+            break
+
+        api_url = paging_list['next_page']
+   
+    return card_list """
+
+def search_api(search):   
+    api_prefix = "https://api.scryfall.com/cards/search?unique=prints&q="
+
+    api_url = api_prefix + f'{search}'
+
+    paging_list = loads(get(api_url).text)
+
+    card_list = []
+
+    while True:
+        paging_list = loads(get(api_url).text)
+        for card in paging_list['data']:
+            #the try/except here is because double face cards dont have the same image_uri path and throws a key error
+            #will i fix this later?
+            try:
                 card_data = {
                     'name': card['name'],
                     'set': card['set_name'],
@@ -22,13 +52,14 @@ def search_api(source):
                     'price': card['prices']['usd'],
                     'image': card['image_uris']['normal']
                 }
-
                 card_list.append(card_data)
+            except KeyError:
+                continue
 
-            if not paging_list['has_more']:
-                break
-
+        if paging_list['has_more']:
             api_url = paging_list['next_page']
-    except:
-            print("source")
+            
+        else:    
+            break
+
     return card_list
